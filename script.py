f7d3e5c0669cc09ef0e7c10a16cfd775c8b4f40b
“jasonjiangstuy” <jasonjiangny@gmail.com>
2022-07-22 10:53:14 -0400

    sync

---
+++ script.py
@@ -0,0 +1,15 @@
+#! /home/jason/TempDiscordApiCaller/env/bin/python3.9
+#-*- coding: utf-8 -*-
+  
+import pickle
+def checkDB():
+    lastdb = pickle.loads( open("lastdb.p", "rb") )
+    TODO: # understand how to pull db from url
+
+
+def run():
+    
+# https://www.geeksforgeeks.org/how-to-schedule-python-scripts-as-cron-jobs-with-crontab/#:~:text=The%20Cron%20job%20utility%20is,scripts%20to%20be%20executed%20periodically.
+  
+if __name__ == '__main__':
+    run()
